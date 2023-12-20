290f9f570f87920e9b5a054eff72cad5a266ceeb
Anthony Drew <adrewa274@ulacit.ed.cr>
2023-12-19 18:53:12 -0600

    aa

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -5,12 +5,12 @@ import 'firebase/compat/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDeIpjKuqgrm9E3DJHNPeE4iNBlVKa-D50",
-    authDomain: "proyectofinal-2d034.firebaseapp.com",
-    projectId: "proyectofinal-2d034",
-    storageBucket: "proyectofinal-2d034.appspot.com",
-    messagingSenderId: "295952897902",
-    appId: "1:295952897902:web:46a83c413c972efb38d37f"
+    apiKey: "AIzaSyCRf7-48LB7SIEz-fgP5NTL_P2oEPAaXe0",
+    authDomain: "proyecto-final-66a17.firebaseapp.com",
+    projectId: "proyecto-final-66a17",
+    storageBucket: "proyecto-final-66a17.appspot.com",
+    messagingSenderId: "342736321010",
+    appId: "1:342736321010:web:b60373ffbad509162d536b"
 };
 
 // Initialize Firebase
